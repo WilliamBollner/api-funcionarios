cb6bbe1655669fcaedb2f2b2de593874ceabaa6f
William Bollner <williambollner@hotmail.com>
2024-06-04 11:05:13 -0300

    Adicionado Bootstrap e alert

--- src/app/funcionario-list/funcionario-list.component.ts
+++ src/app/funcionario-list/funcionario-list.component.ts
@@ -2,18 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { FuncionarioService } from '../services/funcionario.service';
 import { CommonModule, NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-funcionario-list',
   standalone: true,
-  imports: [CommonModule, NgFor, RouterLink],
+  imports: [CommonModule, NgFor, RouterLink, NgbAlertModule],
   templateUrl: './funcionario-list.component.html',
   styleUrl: './funcionario-list.component.scss'
 })
 export class FuncionarioListComponent implements OnInit  {
   funcionarios: any[] = [];
   message: string = '';
-  messageClass: string = '';
 
   constructor(private funcionarioService: FuncionarioService) {}
 
@@ -27,15 +27,11 @@ export class FuncionarioListComponent implements OnInit  {
     });
   }
 
-  editFuncionario(employee: any) {
-    // Implementar navegação para o componente de edição com os dados do funcionário
-  }
-
   deleteFuncionario(id: number) {
     if (confirm('Você tem certeza que deseja excluir este funcionário?')) {
       this.funcionarioService.deleteFuncionario(id).subscribe(response => {
-        this.message = response.mensagem;
-        this.messageClass = response.status === 'Ok' ? 'success' : 'error';
+        this.message = response.status === 'Ok' ? 'success' : 'error';
+        console.log(response.status);
         this.loadFuncionarios();
       });
     }
